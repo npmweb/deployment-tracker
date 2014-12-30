d40e0d252fe59e1bfb34b3ae21b83c16150d9e71
Josh Justice <josh@need-bee.com>
2014-12-30 15:55:32 -0500

    adds top navigation

--- app/views/backend/layouts/_header.blade.php
+++ app/views/backend/layouts/_header.blade.php
@@ -1,29 +1,36 @@
 <div class="contain-to-grid">
-	<nav class="top-bar" data-topbar>
-		<ul class="title-area">
-			<li class="name">
-				<h1>{{ link_to('/', 'DeploymentTracker') }}</h1>
-			</li>
-			<li class="toggle-topbar menu-icon"><a href="#"></a></li>
-		</ul>
-		<section class="top-bar-section">
-			<ul class="right">
-				@if (Auth::check())
-					<li>
-						<a href="#" onclick="$('#logout').submit()">Log Out</a>
-						{{ Form::open(['route'=>['logins.destroy','1'], 'method'=>'delete', 'id'=>'logout']) }}
-						{{ Form::close() }}
-					</li>
-				@endif
-			</ul>
-		</section>
-	</nav>
+    <nav class="top-bar" data-topbar>
+        <ul class="title-area">
+            <li class="name">
+                <h1>{{ link_to('/', 'DeploymentTracker') }}</h1>
+            </li>
+            <li class="toggle-topbar menu-icon"><a href="#"></a></li>
+        </ul>
+        <section class="top-bar-section">
+            <ul class="right">
+                @if (Auth::check())
+                    <li>
+                        <a href="#" onclick="$('#logout').submit()">Log Out</a>
+                        {{ Form::open(['route'=>['logins.destroy','1'], 'method'=>'delete', 'id'=>'logout']) }}
+                        {{ Form::close() }}
+                    </li>
+                @endif
+            </ul>
+
+            <ul class="left">
+                @if (Auth::check())
+                    <li>{{ link_to_route('servers.index', 'Servers') }}</li>
+                    <li>{{ link_to_route('applications.index', 'Applications') }}</li>
+                @endif
+            </ul>
+        </section>
+    </nav>
 </div>
 
 @if ( Session::get('myflash') )
-	<div class="row">
-		<div class="column">
-			<div data-alert class="alert-box">{{ Session::get('myflash') }}</div>
-		</div>
-	</div>
+    <div class="row">
+        <div class="column">
+            <div data-alert class="alert-box">{{ Session::get('myflash') }}</div>
+        </div>
+    </div>
 @endif
